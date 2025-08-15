9db23ec85ce6da86220e0709f116751c5e031761
Stephen Wooten <swooten@frontierrisk.com>
2025-08-15 15:07:36 +0100

    Add design assets and documents

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,6 +1,7 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
+  darkMode: 'class',
   content: [
     './public/*.html',
     './app/helpers/**/*.rb',
@@ -11,6 +12,29 @@ module.exports = {
     extend: {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        sand: '#F7F2EC',
+        clay: '#E5D5C1',
+        coral: '#FF6B5A',
+        amber: '#FFB347',
+        teal: '#2AB7A9',
+        charcoal: '#2E2E2E',
+        warmgray: '#8B8682',
+        charcoalbg: '#1A1A1A',
+        warmblack: '#121010',
+      },
+      fontSize: {
+        h1: ['28px', { lineHeight: '1.2', fontWeight: '700' }],
+        h2: ['22px', { lineHeight: '1.3', fontWeight: '600' }],
+        body: ['16px', { lineHeight: '1.6', fontWeight: '400' }],
+        caption: ['13px', { lineHeight: '1.4', fontWeight: '400' }],
+      },
+      transitionTimingFunction: {
+        'brand': 'cubic-bezier(0.4, 0, 0.2, 1)'
+      },
+      transitionDuration: {
+        brand: '350ms'
       },
     },
   },
